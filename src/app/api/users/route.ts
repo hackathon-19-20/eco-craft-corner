408dc10ec5caa5a61b42e945c215f1f8b48fc91f
Pradneya <pradneya.prog@gmail.com>
2024-01-21 21:56:08 +0530

    Fixed a Bug

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllUsers } from "@/lib/api/users/users";
 import { NextResponse } from "next/server";
 
-export const GET =  async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
+export const GET =  async () => {
   try {
     const data = await getAllUsers();
     return new NextResponse("Here are the users : " + JSON.stringify(data), { status: 200 });
