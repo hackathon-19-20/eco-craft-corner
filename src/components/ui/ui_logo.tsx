b60d6bf586063ddc9f6d301fa40a3ec7c14c8acc
Ankit <rattanankit2004@gmail.com>
2024-01-26 22:14:38 +0530

    Fix error

--- src/components/ui/ui_logo.tsx
+++ src/components/ui/ui_logo.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import img from "../../../public/Logo.png";
 import Link from "next/link";
 
 
@@ -8,7 +7,7 @@ const Logo = () => {
   return (
     <div className="logo ">
       <Link href="/">
-        <Image src={img} alt="Logo" className="h-[6rem] w-[13rem] p-2 mt-5" />
+        <Image src="/Logo.png" alt="Logo" className="h-[6rem] w-[13rem] p-2 mt-5" />
 
       </Link>
 
