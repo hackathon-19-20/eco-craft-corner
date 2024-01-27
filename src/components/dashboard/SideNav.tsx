032c4fdbe899074cb873fe5b6246961ad3bbfcbb
hackathon-19-20 <157271966+hackathon-19-20@users.noreply.github.com>
2024-01-27 12:46:09 +0530

    Update SideNav.tsx

--- src/components/dashboard/SideNav.tsx
+++ src/components/dashboard/SideNav.tsx
@@ -13,7 +13,7 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 md:w-auto">
-          <Image src="/logo.png" alt="logo" width={650} height={500} className="" />
+          <Image src="/Logo.png" alt="logo" width={650} height={500} className="" />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
